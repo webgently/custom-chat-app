15af422e003925d593bc4bc2977c05c04b050b89
webgently <gently039@gmail.com>
2024-12-23 02:23:39 -0800

    update: api client

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -1,40 +1,50 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
+import axios from "axios";
 import { notificationActions } from "../store/notificationSlice";
+
+const token = JSON.parse(localStorage.getItem("token"));
+
+// Create an Axios instance with a base URL
+const axiosInstance = axios.create({
+  baseURL: `${process.env.REACT_APP_API_BASE_URL}/api`, // Replace with your server's base URL
+  headers: {
+    "Content-Type": "application/json",
+    "x-auth-token": token,
+  },
+});
 
 const useFetch = ({ method, url }, successFn, errorFn) => {
   const [requestState, setRequestState] = useState();
   const dispatch = useDispatch();
 
   const requestFunction = async (values) => {
-    const methodUpper = method.toUpperCase();
-    const fetchOptions =
-      methodUpper !== "GET"
-        ? {
-            method: methodUpper,
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(values),
-          }
-        : {};
-
     try {
       setRequestState("loading");
-      const response = await fetch(`/api${url}`, fetchOptions);
-      let data;
-      if (methodUpper !== "DELETE") {
-        data = await response.json();
-      }
-      if (!response.ok) throw new Error(data.message);
+
+      const config = {
+        method: method.toUpperCase(),
+        url,
+        ...(method.toUpperCase() !== "GET" && { data: values }),
+      };
+
+      const response = await axiosInstance(config);
+      const data = response.data;
+
       setRequestState("success");
       successFn && successFn(data);
       return data;
     } catch (error) {
       setRequestState("error");
+
+      const errorMessage =
+        error.response?.data?.message ||
+        error.message ||
+        "Something went wrong";
+
       dispatch(
         notificationActions.addNotification({
-          message: error.message,
+          message: errorMessage,
           type: "error",
         })
       );
